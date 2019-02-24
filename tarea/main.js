0fd4829d9f5a7880d6fbcfcd69a82b5d57c08dfb
Gustavo Tallaferro <gustallafer@gmail.com>
2019-02-24 10:42:53 -0300

    Initial commit

---
+++ tarea/main.js
@@ -0,0 +1,15 @@
+function color1(){
+	var html = document.getElementsByTagName('html')[0];
+	var c1 = document.querySelector('.color1');
+	html.style.backgroundColor = '#F99E4C';
+}
+function color2(){
+	var html = document.getElementsByTagName('html')[0];
+	var c2 = document.querySelector('.color2');
+	html.style.backgroundColor = '#F36F38';
+}
+function color3(){
+	var html = document.getElementsByTagName('html')[0];
+	var c3 = document.querySelector('.color3');
+	html.style.backgroundColor = '#EF4648';
+}
